0b2c8a268075e767d862dae95bc6cb215d01019f
binbandit <crazywolf132@gmail.com>
2025-12-02 20:22:25 +1100

    feat(auth): add scoped login/logout and web flow

--- snpm-core/src/operations/mod.rs
+++ snpm-core/src/operations/mod.rs
@@ -3,7 +3,7 @@ pub mod init;
 pub mod install;
 pub mod run;
 
-pub use auth::login;
+pub use auth::{login, logout};
 pub use init::init;
 pub use install::{InstallOptions, OutdatedEntry, install, outdated, remove, upgrade};
 pub use run::{run_script, run_workspace_scripts};
